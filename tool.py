30722df3579cd06add1d28e2de28b7e8497929f4
Daylily-Zeleen <daylily-zeleen@foxmail.com>
2023-06-30 23:43:05 +0800

    restructure

---
+++ tool.py
@@ -0,0 +1,10 @@
+﻿import os
+
+for root, dirs, files in os.walk("./"):
+    for file in files:
+        if file.endswith(".h"):
+            src_file = os.path.join(root, file)
+            dst_file = src_file.removesuffix("pp")
+            os.rename(src_file, dst_file)
+
+print("finish")
